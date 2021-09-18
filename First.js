91507c953a49926c6946c689ad1d9d56fa3bca4f
habeeb <habeeb@habeebs-MacBook-Air.local>
2021-09-18 22:17:15 +1000

    day1 js

---
+++ First.js
@@ -0,0 +1,22 @@
+console.log(29+3.5); // gives you output from the parenthesis 
+//console.log("Aslam's age is 28"); // quotations used only for strings
+// DATA TYPES
+//console.log("javascript");
+//Arthematic operators
+console.log(2021-1969);
+console.log(65/240);
+console.log(0.2708*100);
+
+// String concatenation
+console.log('hi'+'5');
+console.log('helloDuniyaa');
+
+console.log(('hello'+' '+ 'Duniyaa').length);
+
+// Methods
+console.log('aslam'.toUpperCase());
+
+// to remove white spaces only between the quotations
+console.log('  Aslam  '+'  Habeeb  ');
+console.log(('      AslamHabeeb  ').trim());
+
